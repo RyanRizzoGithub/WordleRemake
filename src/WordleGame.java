e47ddee901931486b7b00c8fbcb24bee004324da
Ryan Rizzo <89487878+RyanRizzoGithub@users.noreply.github.com>
2022-11-22 09:25:07 -0700

    Add files via upload

---
+++ src/WordleGame.java
@@ -0,0 +1,144 @@
+// import java.util.ArrayList;
+
+// @Katelen Tellez added (below)
+public class WordleGame {
+	
+	// private HashMap dictionary;
+	private String word;
+	// private ArrayList usedWords;
+	private String guess;
+	private int guessNum;
+	private boolean guessCorrect;
+	private int [] guessResults;
+	private boolean [] charFoundStatus = new boolean [5];;
+	
+	public WordleGame() {
+		
+		// initialize dictionary?
+		// generate random word from dictionary (no repeats)
+		
+		word = "PARTY";
+		guess = "";
+		guessNum = 0;
+		guessCorrect = false;
+		
+		// prevents double chars from being found when there are none
+		for(int i = 0; i < 5; i++)
+			charFoundStatus[i] = false;
+		
+		// a 0 indicates character is in the word and in the right index
+		// a 1 indicates character is in the word but not in the right index
+		// a -1 indicates character is not in the word
+		guessResults = new int [5];
+	}
+	
+	/*
+	private String getRandWord(HashMap dict) {
+		// generate random word from dictionary
+		// check if it's been used in this session
+		// if not, add to usedWords
+		// if it has, generate new word
+		// return generated word
+	}
+	*/
+	
+	public int[] makeAGuess(char[][] input) {
+		
+		// convert input chars into a string
+		for (int i=0; i<5; i++) {
+			
+			// input at row guessNum so we know which is the current row to look at
+			guess = guess + Character.toString(input[i][guessNum]).toUpperCase();
+		}
+		
+		System.out.println("Guess: " + guess);
+		
+		// did the user guess the word correctly?
+		if(guess.equals(word)) {
+			guessCorrect = true;
+			for(int i = 0; i < 5; i++) {
+				guessResults[i] = 0;
+				charFoundStatus[i] = true;
+			}
+		}
+		
+		// if they did not, compare each character of the guess to the word
+		else {
+		
+			for(int guessIdx = 0; guessIdx < 5; guessIdx++) {
+				
+				char guessChar = guess.charAt(guessIdx);
+				
+				for(int wordIdx = 0; wordIdx < 5; wordIdx++) {
+					
+					char wordChar = word.charAt(wordIdx);
+    				
+					if((wordChar == guessChar) && (wordIdx == guessIdx)) {
+						
+						if(charFoundStatus[wordIdx] == false) {
+							guessResults[guessIdx] = 0;
+							charFoundStatus[wordIdx] = true;
+							break;
+						}
+					}
+						
+					else if(wordChar == guessChar) {
+						
+						if(charFoundStatus[wordIdx] == false) {
+							guessResults[guessIdx] = 1;
+							charFoundStatus[wordIdx] = true;
+							break;
+						}
+					}
+					
+					else if(charFoundStatus[wordIdx] == false){
+						guessResults[guessIdx] = -1;
+					}
+							
+				}
+				
+			}
+			
+		}
+		guessNum++;
+		
+		// reset the found status of each character
+		for(int i = 0; i < 5; i++)
+			charFoundStatus[i] = false;
+		
+		// reset the guess
+		guess = "";
+		
+		// return the results of the guess
+		return guessResults;
+	}
+	
+	public boolean gameIsOver() {
+		
+		if(guessNum == 5) {
+			return true;
+		}
+		return guessCorrect;
+	}
+	
+	public boolean guessIsCorrect() {
+		return guessCorrect;
+	}
+	/*
+	 * Ryan Rizzo
+	 */
+	public int checkChar(char c, int index) {
+		c = Character.toUpperCase(c);
+		
+		int result = -1;
+		for (int i=0; i<word.length(); i++) {
+			if (word.charAt(i) == c) {
+				result++;
+				break;
+			}
+		} if (word.charAt(index) == c) {
+			result++;
+		}
+		return result;
+	}
+}
--- src/WordleUI.java
+++ src/WordleUI.java
@@ -1,9 +1,14 @@
+import java.util.LinkedList;
+import java.util.Random;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Rectangle;
@@ -12,26 +17,51 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class WordleUI {
 	private Display display;
-	private Shell shell;
+	private Shell gameShell;
+	private Shell menuShell;
+	private Shell profileShell;
 	private Canvas canvas;
 	private int row;
 	private int col;
 	private char[][] input;
 	private boolean[] rowSubmitted;
+	private boolean[] hovered;
+	
+
+	// @Katelen Tellez added
+		private WordleGame game;
+		private int [] guessResults = null;
+		private int[][] allGuesses = new int [6][5];
+		private int guessNum = 0;
 	
 	
-	public WordleUI () {
+	public WordleUI (WordleGame currGame) {
+		game = currGame;
+		
 		Display.setAppName("Wordle");
 		display = new Display();  
-		shell = new Shell(display);
-	    shell.setText("Wordle");
-		shell.setSize(600,1000);
-		shell.setLayout( new GridLayout());	
+		gameShell = new Shell(display);
+	    gameShell.setText("Wordle");
+		gameShell.setSize(600,1000);
+		gameShell.setLayout( new GridLayout());	
+		
+		menuShell = new Shell(display);
+		menuShell.setText("WordleMenu");
+		menuShell.setSize(600, 1000);
+		menuShell.setLayout( new GridLayout());
+		
+		profileShell = new Shell(display);
+		profileShell.setText("WordleProfile");
+		profileShell.setSize(400, 400);
+		profileShell.setLayout(new GridLayout());
+		
 		row = 0;
 		col = 0;
+		
 		input = new char[5][6];
 		for (int i=0; i<5; i++) {
 			for (int j=0; j<6; j++) {
@@ -39,14 +69,190 @@ public class WordleUI {
 			}
 		}
 		rowSubmitted = new boolean[6];
+		hovered = new boolean[6];
 		for (int i=0; i<6; i++) {
 			rowSubmitted[i] = false;
+			hovered[i] = false;
 		}
+		
 	}
 	
-	public void start() {
-		Composite upperComp = new Composite(shell, SWT.NO_FOCUS);
-	    Composite lowerComp = new Composite(shell, SWT.NO_FOCUS);
+	public void startMenu() {
+		Composite menuComp = new Composite(menuShell, SWT.NO_FOCUS);
+		
+		canvas = new Canvas(menuComp, SWT.NONE);
+		canvas.setSize(600, 1000);
+		
+		canvas.addPaintListener(e -> {
+			// Set the color of the background and font
+			e.gc.setBackground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
+			
+			
+			// Draw the user profile
+			Font font = new Font(menuShell.getDisplay(), new FontData("Times New Roman", 18, SWT.NONE));
+			e.gc.setFont(font);
+			e.gc.drawText("Logged in as: Guest", 228, 65, true);
+			e.gc.drawRoundRectangle(250, 100, 100, 100, 15, 15);
+			e.gc.drawRoundRectangle(251, 101, 98, 98, 15, 15);
+			
+			
+			// Draw the title
+			font = new Font(menuShell.getDisplay(), new FontData("Times New Roman", 40, SWT.BOLD));
+			e.gc.setFont(font);
+			e.gc.drawText("Wordle", 234, 5, true);
+			
+			
+			// Play button
+			e.gc.drawRoundRectangle(200, 230, 200, 50, 10, 10);
+			if (hovered[0]) {
+				e.gc.fillRoundRectangle(201, 231, 198, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(201, 231, 198, 48, 10, 10);
+			}
+			e.gc.drawText("Play!", 260, 230, true);
+			
+			font = new Font(menuShell.getDisplay(), new FontData("Times New Roman", 30, SWT.BOLD));
+			e.gc.setFont(font);
+			
+			
+			// Log in button	
+			e.gc.drawRoundRectangle(210, 300, 180, 50, 10, 10);
+			if (hovered[1]) {
+				e.gc.fillRoundRectangle(211, 301, 178, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(211, 301, 178, 48, 10, 10);
+			}
+			e.gc.drawText("Login", 264, 308, true);
+			
+			
+			// Mode button
+			e.gc.drawRoundRectangle(210, 370, 180, 50, 10, 10);
+			if (hovered[2]) {
+				e.gc.fillRoundRectangle(211, 371, 178, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(211, 371, 178, 48, 10, 10);
+			}
+			e.gc.drawText("Mode", 264, 378, true);
+			
+			
+			// Leader board button
+			e.gc.drawRoundRectangle(210, 440, 180, 50, 10, 10);
+			if (hovered[3]) {
+				e.gc.fillRoundRectangle(211, 441, 178, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(211, 441, 178, 48, 10, 10);
+			}
+			e.gc.drawText("Leaderboard", 217, 448, true);
+			
+			
+			// Theme selection button
+			e.gc.drawRoundRectangle(210, 510, 180, 50, 10, 10);
+			if (hovered[4]) {
+				e.gc.fillRoundRectangle(211, 511, 178, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(211, 511, 178, 48, 10, 10);
+			}
+			e.gc.drawText("Theme", 258, 518, true);
+			
+			
+			// Help page button
+			e.gc.drawRoundRectangle(210, 580, 180, 50, 10, 10);
+			if (hovered[5]) {
+				e.gc.fillRoundRectangle(211, 581, 178, 48, 10, 10);
+			} else {
+				e.gc.drawRoundRectangle(211, 581, 178, 48, 10, 10);
+			}
+			e.gc.drawText("Help", 270, 588, true);
+			
+			
+		});
+		
+		// Add mouse move listener to highlight buttons once hovered
+		canvas.addMouseMoveListener(new MouseMoveListener() {
+			@Override
+			public void mouseMove(MouseEvent e) {
+				//System.out.println("x: " + e.x + " y: " + e.y);
+				
+				// If Play button
+				if (e.x > 200 && e.x < 400 && e.y > 230 && e.y < 280) {
+					//System.out.println("Play!");
+					hovered[0] = true;
+				}
+				
+				// If Login button
+				else if (e.x > 210 && e.x < 390 && e.y > 300 && e.y < 350) {
+					//System.out.println("Login");
+					hovered[1] = true;
+				}
+				
+				// If Mode button
+				else if (e.x > 210 && e.x < 390 && e.y > 370 && e.y < 420) {
+					//System.out.println("Mode");
+					hovered[2] = true;
+				}
+				
+				// If leader board button
+				else if (e.x > 210 && e.x < 390 && e.y > 440 && e.y < 490) {
+					//System.out.println("Leaderboard");
+					hovered[3] = true;
+				}
+				
+				// If theme button
+				else if (e.x > 210 && e.x < 390 && e.y > 510 && e.y < 560) {
+					//System.out.println("Theme");
+					hovered[4] = true;
+				}
+				
+				// If help button
+				else if (e.x > 210 && e.x < 390 && e.y > 580 && e.y < 630) {
+					//System.out.println("Help");
+					hovered[5] = true;
+				}
+				
+				// Reset hovered array
+				else {
+					for (int i=0; i<6; i++) {
+						hovered[i] = false;
+					}
+				}
+				canvas.redraw();
+			}
+		});
+		
+		canvas.addMouseListener(new MouseListener() {
+			
+			public void mouseDown(MouseEvent e) {
+				// If play button
+				if (e.x > 200 && e.x < 400 && e.y > 230 && e.y < 280) {
+					menuShell.setVisible(false);;
+				}
+				
+				// If Login button
+				else if (e.x > 210 && e.x < 390 && e.y > 300 && e.y < 350) {
+					//System.out.println("Login");
+					startLogin();
+				}
+				
+				canvas.redraw();
+			}
+			
+			public void mouseDoubleClick(MouseEvent e) {}
+
+			public void mouseUp(MouseEvent e) {}
+			
+		});
+		
+		
+		// Event loop
+		menuShell.open();
+		while(menuShell.isVisible())
+			if(!display.readAndDispatch()) {		
+			}
+	}
+	
+	public void startGame() {
+		Composite upperComp = new Composite(gameShell, SWT.NO_FOCUS);
+	    //Composite lowerComp = new Composite(gameShell, SWT.NO_FOCUS);
 		
 		canvas = new Canvas(upperComp, SWT.NONE);
 		canvas.setSize(600, 1000);
@@ -57,7 +263,7 @@ public class WordleUI {
 			e.gc.setBackground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
 			
 			// Draw the title
-			Font font = new Font(shell.getDisplay(), new FontData("Times New Roman", 40, SWT.BOLD));
+			Font font = new Font(gameShell.getDisplay(), new FontData("Times New Roman", 40, SWT.BOLD));
 			e.gc.setFont(font);
 			e.gc.drawText("Wordle", 234, 5, true);
 			
@@ -78,6 +284,30 @@ public class WordleUI {
 	        				row++;
 	        				col = 0;
 	        				
+	        				/*
+	        				// @Katelen Tellez added
+	        				// process input in the WORDLE class
+	        				guessResults = game.makeAGuess(input);
+	        				for(int i = 0; i < 5; i ++) {
+	        					allGuesses[guessNum][i] = guessResults[i];
+	        				}
+	        				guessNum++;
+	        				
+	        				System.out.println("Guess Results: ");
+	        				for(int g = 0; g < 5; g++) {
+	        					System.out.println(guessResults[g]);
+	        				}
+	        				
+	        				System.out.print("All Guesses: ");
+	        				for(int g = 0; g < 6; g++) {
+	        					
+	        					for(int c = 0; c < 5; c++) {
+	        						System.out.print(allGuesses[g][c] + " ");
+	        					}
+	        					System.out.println();
+	        				}
+	        				System.out.println();
+	        				*/
 	        			}
 	        			
 	        		// If DELETE key
@@ -157,20 +387,42 @@ public class WordleUI {
 				}
 				canvas.redraw();
 			}
-			
 			public void mouseDoubleClick(MouseEvent e) {}
-
 			public void mouseUp(MouseEvent e) {}
 			 
 		 });
 		
 		// Event loop
-		shell.open();
-		while( !shell.isDisposed())
+		gameShell.open();
+		while( !gameShell.isDisposed())
 			if(!display.readAndDispatch()) {		
 			}
 		display.dispose();
 	}
+	
+	public void startLogin() {
+		Composite profileComp = new Composite(profileShell, SWT.NO_FOCUS);
+		canvas = new Canvas(profileComp, SWT.NONE);
+		canvas.setSize(400, 400);
+		
+		canvas.addPaintListener(e -> {
+			Font font = new Font(profileShell.getDisplay(), new FontData("Times New Roman", 20, SWT.NONE));
+			e.gc.setFont(font);
+			e.gc.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
+			e.gc.drawText("Login: ", 20, 10);
+			
+			Text loginUser = new Text(profileShell, SWT.BORDER);
+			loginUser.setLocation(20, 30);
+			loginUser.setText("Username");
+		});
+		
+		// Event loop
+		profileShell.open();
+		while(profileShell.isVisible())
+			if(!display.readAndDispatch()) {		
+			}
+	}
+	
 	
 	/* - - - - - - DRAW USER INPUT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	 * This function is responsible displaying each word that the user has guessed on the
@@ -180,8 +432,11 @@ public class WordleUI {
 	 * (Ryan Rizzo)
 	 */
 	private void drawUserInput(PaintEvent e) {
+		// Save color
+		Color currColor = e.gc.getForeground();
+		
 		// Set the correct font
-		Font font = new Font(shell.getDisplay(), new FontData("Times New Roman", 40, SWT.BOLD));
+		Font font = new Font(gameShell.getDisplay(), new FontData("Times New Roman", 40, SWT.BOLD));
 		e.gc.setFont(font);		
 		
 		
@@ -205,6 +460,8 @@ public class WordleUI {
 				}
 			}
 		}
+		// Restore color
+		e.gc.setForeground(currColor);
 	}
 	
 	
@@ -216,42 +473,41 @@ public class WordleUI {
 	 * (Ryan Rizzo)
 	 */
 	private void drawInputRectangles(PaintEvent e) {
-		// Set the color of the rectangles
-		e.gc.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
+		// Save color
+		Color currColor = e.gc.getForeground();
 		
 		// Iterate over 5 columns and 6 rows
 		for (int i=0; i<5; i++) {
 			for (int j=0; j<6; j++) {
-				// Draw the square
-				Rectangle rect = new Rectangle(120 + (70 * i), 60 + (70 * j), 60, 60);
-				e.gc.drawRectangle(rect);
+				
 				// Draw a smaller square to represent character info
 				if (rowSubmitted[j] == true) {
-					// TODO: get val from character
-					// if (getRelevence(input[i][j]) == 0 ) {
-					//		e.gc.setForeground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
-					// }
-					// if (getRelvence(input[i][j] == 1 ) {
-					//		e.gc.setForeground(display.getSystemColor(SWT.COLOR_YELLOW));
-					// }
-					// if (getRelevence(input[i][j] == 2 ) {
-					// 		e.gc.setForeground(display.getSystemColor(SWT.COLOR_GREEN));
-					// }
-					// rect = new Rectangle(121 + (70 * i), 61 + (70 * j), 58, 58);
-					// e.gc.fillRectangle(rect);
-					
-					// Temporary
-					e.gc.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
-					rect = new Rectangle(121 + (70 * i), 61 + (70 * j), 58, 58);
-					e.gc.fillRectangle(rect);
-					
+					 if (game.checkChar(input[i][j], i) == -1 ) {
+							e.gc.setForeground(display.getSystemColor(SWT.COLOR_DARK_GRAY));
+					 }
+					 if (game.checkChar(input[i][j], i) == 0 ) {
+							e.gc.setForeground(display.getSystemColor(SWT.COLOR_YELLOW));
+					 }
+					 if (game.checkChar(input[i][j], i) == 1 ) {
+					 		e.gc.setForeground(display.getSystemColor(SWT.COLOR_GREEN));
+					 }
 				} else {
-					e.gc.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
-					rect = new Rectangle(121 + (70 * i), 61 + (70 * j), 58, 58);
-					e.gc.drawRectangle(rect);
+					e.gc.setForeground(display.getSystemColor(SWT.COLOR_BLACK));
 				}
+				// Draw inner square
+				Rectangle rect = new Rectangle(121 + (70 * i), 61 + (70 * j), 58, 58);
+				e.gc.fillRectangle(rect);
+				
+				// Draw the outter square
+				rect = new Rectangle(120 + (70 * i), 60 + (70 * j), 60, 60);
+				e.gc.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
+				e.gc.drawRectangle(rect);				
 			}
 		}
+		// Restore color
+		e.gc.setForeground(currColor);
+		
+		canvas.redraw();
 	}
 	
 	/* - - - - - - DRAW KEYBOARD - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -261,6 +517,9 @@ public class WordleUI {
 	 * (Ryan Rizzo)
 	 */
 	private void drawKeyboard(PaintEvent e) {
+		// Save color
+		Color currColor = e.gc.getForeground();
+		
 		// Create a index variable
 		int i=0;
 		
@@ -269,7 +528,7 @@ public class WordleUI {
 				"j","k","l","z","x","c","v","b","n","m","`"};
 		
 		// Setup the font for each character
-		Font font = new Font(shell.getDisplay(), new FontData("Times New Roman", 18, SWT.BOLD));
+		Font font = new Font(gameShell.getDisplay(), new FontData("Times New Roman", 18, SWT.BOLD));
 		e.gc.setFont(font);
 		e.gc.setForeground(display.getSystemColor(SWT.COLOR_WHITE));
 		
@@ -295,7 +554,7 @@ public class WordleUI {
 		}
 		
 		// Setup font for Enter & Delete
-		font = new Font(shell.getDisplay(), new FontData("Times New Roman", 14, SWT.BOLD));
+		font = new Font(gameShell.getDisplay(), new FontData("Times New Roman", 14, SWT.BOLD));
 		e.gc.setFont(font);
 		
 		// Draw the Enter key
@@ -305,5 +564,37 @@ public class WordleUI {
 		// Draw the Delete key
 		e.gc.fillRoundRectangle(448, 620, 64, 50, 10, 10);
 		e.gc.drawText("DELETE", 452, 638, true);
+		
+		// Restore color
+		e.gc.setForeground(currColor);
+	}
+	
+	/* - - - - - - DRAW ANIMATION - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	 * This function is responsible displaying background falling letters animation
+	 * 
+	 * @param e, the paint event which calls this function
+	 * (Ryan Rizzo)
+	 */
+	private void drawAnimation(PaintEvent e) {
+		new Thread(new Runnable() {
+			int time = 0;
+			public void run() {
+				Random rand = new Random();
+				String[] qwerty = {"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h",
+						"j","k","l","z","x","c","v","b","n","m","`"};
+				LinkedList<String[]> memo = new LinkedList<String[]>();
+				for (int i = 0; i < 100; i++) {
+					String selectChar = qwerty[rand.nextInt(0, 27)];
+					String selectX = String.valueOf(rand.nextInt(0, 600));
+					String order = String.valueOf(i);
+					String[] memoEntry = {selectChar, selectX, "0", order};
+					memo.add(memoEntry);
+				}
+				
+				time++;
+			}
+		}).start();
 	}
 }
+
+
