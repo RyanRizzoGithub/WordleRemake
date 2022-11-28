4ae49952c4b505083511534241d8ebc492f56666
GregoryJenkinsUofA <87399251+GregoryJenkinsUofA@users.noreply.github.com>
2022-11-27 21:48:27 -0700

    Added test for Player Comparable

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
@@ -45,6 +48,54 @@ class PlayerTest {
 		assertTrue(test.getMaxStreak() == 1);
 		assertEquals(50.0, test.getWinPercentage());
 		
+	}
+	
+	@Test
+	void sortList() {
+		ArrayList<Player> test = new ArrayList<>();
+		
+		Player test1 = new Player("test1");
+		Player test2 = new Player("test2");
+		Player test3 = new Player("test3");
+		Player test4 = new Player("test4");
+		Player test5 = new Player("test5");
+		
+		for (int i = 0; i < 5; i++) {
+			test1.addGame(true, i);
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			test2.addGame(true, i);
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			test3.addGame(true, i);
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			test4.addGame(true, i);
+		}
+		
+		for (int i = 0; i < 1; i++) {
+			test5.addGame(true, i);
+		}
+		
+		test.add(test4);
+		test.add(test1);
+		test.add(test5);
+		test.add(test3);
+		test.add(test2);
+		
+		
+		
+		Collections.sort(test);
+		
+		assertEquals(test5, test.get(0));
+		assertEquals(test4, test.get(1));
+		assertEquals(test3, test.get(2));
+		assertEquals(test2, test.get(3));
+		assertEquals(test1, test.get(4));
+
 	}
 
 }
