0233c73e525bdfd3749d7d5beb8b658f20884406
Ryan Rizzo <89487878+RyanRizzoGithub@users.noreply.github.com>
2022-12-06 12:33:18 -0700

    comments

--- src/WordleModeUI.java
+++ src/WordleModeUI.java
@@ -9,26 +9,20 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.wb.swt.SWTResourceManager;
 
+/**
+ * AUTHOR(S):	Ryan Rizzo
+ * FILE:		WordleModeUI.java
+ * CLASS:		CSC 335 - Final Project
+ * DATE:		12/6/22
+ * PURPOSE:		Responsible for changing the game mode
+ */
 public class WordleModeUI {
-
 	protected Shell shell;
 	private static int modeSelected = 0;
 
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			WordleModeUI window = new WordleModeUI();
-			window.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Open the window.
+	/** - - - - - - START - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	 *  Generates the content and displays the mode selection screen
+	 *  @author Ryan Rizzo
 	 */
 	public void start() {
 		Display display = Display.getDefault();
@@ -42,19 +36,23 @@ public class WordleModeUI {
 		}
 	}
 
-	/**
-	 * Create contents of the window.
+	/** - - - - - - CREATE CONTENTS - - - - - - - - - - - - - - - - - - - - - - - -
+	 * Responsible for adding all of the SWT widgets to the mode selection ui
+	 * @author Ryan Rizzo
 	 */
 	protected void createContents() {
+		// Setup the shell
 		shell = new Shell();
 		shell.setSize(431, 196);
 		shell.setText("SWT Application");
 		shell.setLayout(new GridLayout(2, false));
 		
+		// Add label for mode
 		Label lblMode = new Label(shell, SWT.NONE);
 		lblMode.setFont(SWTResourceManager.getFont(".AppleSystemUIFont", 14, SWT.BOLD));
 		lblMode.setText("Mode...");
 		
+		// Add label for description
 		Label lblDescription = new Label(shell, SWT.NONE);
 		lblDescription.setFont(SWTResourceManager.getFont(".AppleSystemUIFont", 14, SWT.BOLD));
 		
@@ -63,29 +61,36 @@ public class WordleModeUI {
 		lblDescription.setLayoutData(gd_lblDescription);
 		lblDescription.setText("Description...");
 		
+		// Add radio button for Word of The Day
 		Button btnWotd = new Button(shell, SWT.RADIO);
 		btnWotd.setText("WOTD");
 		
+		// Add label for Word of The Day
 		Label lblwordOfThe = new Label(shell, SWT.NONE);
 		lblwordOfThe.setText("\"Word of The Day\" is the classic wordle gamemode ");
 		
+		// Add radio button for survival
 		Button btnSurvival = new Button(shell, SWT.RADIO);
 		btnSurvival.setText("Survival");
 		
+		// Add label for survival description
 		Label lblSeeHowMany = new Label(shell, SWT.NONE);
 		lblSeeHowMany.setText("See how many words you can get in a row!");
 		
+		// Add button for dordle
 		Button btnMode = new Button(shell, SWT.RADIO);
 		btnMode.setText("Dordle");
 		
+		// Add label for dordle description
 		Label lblTryToGuess = new Label(shell, SWT.NONE);
 		lblTryToGuess.setText("Try to guess two words with 7 guesses");
 		
+		// Add button to submit selection
 		Button btnSubmit = new Button(shell, SWT.NONE);
 		btnSubmit.setText("Submit");
 		new Label(shell, SWT.NONE);
 		
-		
+		// Add selection listener to the submission button
 		btnSubmit.addSelectionListener(new SelectionListener() {
 			public void widgetDefaultSelected(SelectionEvent e) {}
 			public void widgetSelected(SelectionEvent e) {
@@ -104,6 +109,11 @@ public class WordleModeUI {
 
 	}
 	
+	/** - - - - - - GET MODE - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	 * Returns the mode selected by the user
+	 * @return String mode
+	 * @author Ryan Rizzo
+	 */
 	public static String getMode() {
 		String mode = "WOTD";
 		if (modeSelected == 1) {
